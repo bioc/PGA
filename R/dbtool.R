32e5a89b5033eb8477b208d6719c853c0c7267dc
shawn <xsh.skye@gmail.com>
2018-05-10 02:26:14 +0800

    1) add the support for Hisat2 splice site; 2) fix a minor bug in report

--- R/dbtool.R
+++ R/dbtool.R
@@ -1,3 +1,37 @@
+## This function requires the Tab format file from Hisat2. Read Tab file contain junctions into a GRanges object.
+
+Tab2Range <- function(tabfile)
+{   
+	options(stringsAsFactors=FALSE)
+	jun <- read.table(tabfile, sep='\t', header=F, stringsAsFactors=F)
+
+	#replace the strand "." with "+" and "-".
+	tmp<-jun[jun$V4==".",]
+	tmp_forward<-tmp
+	tmp_reverse<-tmp
+	tmp_forward$V4<-"+"
+	tmp_reverse$V4<-"-"
+	jun<-rbind(jun[jun$V4!=".",],tmp_forward,tmp_reverse)
+
+	part1_sta <- as.numeric(jun[,'V2'])+1-59
+	part1_end <- as.numeric(jun[,'V2'])+1
+	part2_sta <- as.numeric(jun[,'V3'])+1
+	part2_end <- as.numeric(jun[,'V3'])+1+59
+
+	junction <- data.frame(chr=jun[, 'V1'], id="NULL", start=jun[, 'V2'], 
+		end=jun[,'V3'], cov=255, strand=jun$V4, part1_len=0, 
+		part2_len=0, part1_sta, part1_end, part2_sta, part2_end)
+	if('chrM' %in% junction$chr) junction <- junction[-which(junction$chr=='chrM'), ]
+	if('MT' %in% junction$chr) junction <- junction[-which(junction$chr=='MT'), ]
+
+	junRange <- GRanges(seqnames=junction$chr, ranges=IRanges(start=junction$part1_end, 
+			end=junction$part2_sta), strand=junction$strand, id=junction$id,
+			cov=junction$cov, part1_len=junction$part1_len, part2_len=junction$part2_len, 
+			part1_sta=junction$part1_sta, part1_end=junction$part1_end, 
+			part2_sta=junction$part2_sta, part2_end=junction$part2_end)
+
+}
+
 
 Outputaberrant2 <- function(positiontab, outfa, outmtab, coding, proteinseq, 
                             ids, RPKM=NULL, ...){
@@ -93,7 +127,7 @@ Outputaberrant2 <- function(positiontab, outfa, outmtab, coding, proteinseq,
 
 OutputNovelJun2 <- function(junction_type, genome, outfa,outmtab, 
                             #outfile_c, 
-                            proteinseq, ...){
+                            proteinseq, debug=FALSE, ...){
     options(stringsAsFactors=FALSE)
     #ids <- subset(ids,pro_name!='')
     
@@ -136,12 +170,21 @@ OutputNovelJun2 <- function(junction_type, genome, outfa,outmtab,
     ###already did reverseComplement
     junseq1 <- getSeq(genome, junRange1)
     junseq2 <- getSeq(genome, junRange2)
+
     
     junseq_cat <- DNAStringSet(mapply(function(x, y, z) 
         ifelse(z == '+', paste(x, y, sep=''), paste(y, x, sep='')), 
         as.data.frame(junseq1)[, 1], 
         as.data.frame(junseq2)[, 1], as.character(strand(junRange1))))#
     
+    #debug
+    debug=T
+    if(debug){
+        debug_file<-data.frame(seqnames=novel_junc$seqnames, part1_sta=novel_junc$part1_sta,
+                           part1_end=novel_junc$part1_end,part2_sta=novel_junc$part2_sta,
+                           part1_end=novel_junc$part2_end,strand=novel_junc$strand, seq1=junseq1, seq2=junseq2, seqcat=junseq_cat)
+        write.table(debug_file,file="debug_file.tsv",quote = F, row.names=F)    
+    }
     #index_plus <- which(strand(junRange1) == '+')
     #index_minus <- which(strand(junRange1) == '-')
     #seqs_plus <- junseq_cat[index_plus]
