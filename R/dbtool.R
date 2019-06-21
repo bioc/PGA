1858478bac328952cda6d9f0a275fd0c6d50e13f
Shaohang Xu <xsh.skye@gmail.com>
2019-06-21 17:13:29 +0800

    Implementation for the protein database construction from fusion events

--- R/dbtool.R
+++ R/dbtool.R
@@ -176,9 +176,9 @@ OutputNovelJun2 <- function(junction_type, genome, outfa,outmtab,
         ifelse(z == '+', paste(x, y, sep=''), paste(y, x, sep='')), 
         as.data.frame(junseq1)[, 1], 
         as.data.frame(junseq2)[, 1], as.character(strand(junRange1))))#
-    
-    #debug
-    debug=T
+   
+	#debug
+	debug=T
     if(debug){
         debug_file<-data.frame(seqnames=novel_junc$seqnames, part1_sta=novel_junc$part1_sta,
                            part1_end=novel_junc$part1_end,part2_sta=novel_junc$part2_sta,
@@ -733,8 +733,10 @@ PrepareAnnotationRefseq2 <- function(genome='hg19', CDSfasta, pepfasta,
 
     query_refGene <- ucscTableQuery(session, "refSeqComposite", table="refGene")
     refGene <- getTable(query_refGene)
-    refGene <- subset(refGene, name %in% transcript_ids)
-    
+	if(!is.null(transcript_ids))
+	{
+    	refGene <- subset(refGene, name %in% transcript_ids)
+	}
     reflink <- .UCSC_dbselect("hgFixed", "refLink")
     ids <- subset(reflink, mrnaAcc %in% refGene[, 'name'], select = name:protAcc)
     colnames(ids) <- c('gene_name', 'description', 'tx_name', 'pro_name')
